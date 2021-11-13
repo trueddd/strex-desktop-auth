1fa5db6f747449239ef2be6a51721ed2443a8dd3
trueddd <dmitry.kuzmich@orangesoft.co>
2021-11-13 23:49:17 +0300

    gitignore update and minor fixes

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,16 +1,16 @@
 import io.ktor.client.*
 import io.ktor.client.request.*
-import kotlinx.html.div
-import kotlinx.html.dom.append
-import org.w3c.dom.Node
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.html.div
+import kotlinx.html.dom.append
+import org.w3c.dom.Node
 
 fun main() {
     window.onload = {
-        document.body?.sayHello()
+        document.body?.showUserInfo()
         sendTokenToApp()
     }
 }
@@ -22,7 +22,7 @@ fun sendTokenToApp() {
     }
 }
 
-fun Node.sayHello() {
+fun Node.showUserInfo() {
     append {
         div {
             + "You may now close this page."
